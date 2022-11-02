f609bab45d6d7ce6b2cd22bb335d7911f3a1c7f1
Josh Picarazzi <joshpicarazzi@gmail.com>
2022-11-01 20:25:22 -0500

    Direction keys
    print which direction key is being pressed via debug logs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -9,12 +10,26 @@ import (
 
 type Game struct{}
 
+var dir = "no dir"
+
 func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+		dir = "right"
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+		dir = "left"
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		dir = "down"
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		dir = "up"
+	} else {
+		dir = "no dir"
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.Fill(color.RGBA{0x50, 0x50, 0x50, 0xFF})
+	ebitenutil.DebugPrint(screen, dir)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
